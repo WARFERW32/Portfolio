597603d49b8879f2864f1661f89028c7c9d42e4c
vasyl <vasilijpodlevskij@gmail.com>
2023-02-09 10:17:55 +0100

    nav and header

---
+++ project.js
@@ -0,0 +1,67 @@
+let projectData = [
+ {
+    image: 'img/project-1.png',
+    name: 'project one',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-2.png',
+    name: 'project two',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-3.png',
+    name: 'project three',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-4.png',
+    name: 'project four',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-5.png',
+    name: 'project five',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-6.png',
+    name: 'project six',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-7.png',
+    name: 'project seven',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+ {
+    image: 'img/project-8.png',
+    name: 'project eight',
+    detail: 'Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+    github: '#',
+    live: '#',
+    tags: '#javascript, #fullstack, #css'
+ },
+
+]
